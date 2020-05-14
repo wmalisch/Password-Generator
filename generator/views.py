d238459b430a04d744974e3e0e3dff9a481d0225
Will Malisch <malisch.will@gmail.com>
2020-05-14 13:11:10 -0400

    Finished the application with styling and an extra page

--- generator/views.py
+++ generator/views.py
@@ -5,22 +5,19 @@ import random
 def home(request):
     return render(request, './generator/home.html')
 
+def about(request):
+    return render(request, './generator/about.html')
 
 def password(request):
-
     characters = list('abcdefghijklmnopqrutuvwxyz')
-
     if request.GET.get('uppercase'):
         characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVYXWZ'))
     if request.GET.get('special'):
         characters.extend(list('!@#$%^&*()?<>;:+=-_'))
     if request.GET.get('numbers'):
         characters.extend(list('1234567890'))
-
     length = int(request.GET.get('length',12))
-
     thepassword = ''
     for x in range(length):
         thepassword += random.choice(characters)
-
     return render(request, 'generator/password.html', {'password':thepassword})
