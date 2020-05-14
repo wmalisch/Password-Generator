cf2142ddbb93480ebc9477b2bb501e7dfc818b4c
Will Malisch <malisch.will@gmail.com>
2020-05-14 12:42:01 -0400

    Completed the apps functional parts, still need to finish the styling

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def home(request):
-    return render(request, './generator/home.html', {'password':'hui43g6iu3o4'})
+    return render(request, './generator/home.html')
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list('abcdefghijklmnopqrutuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVYXWZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()?<>;:+=-_'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length',12))
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password':thepassword})
